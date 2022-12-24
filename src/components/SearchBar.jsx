1a3c50b27f35c1712a41211df389ecd43d493f57
Aariyan Apu <aariyan.apu@gmail.com>
2022-12-24 12:42:22 +0600

    change  search font svg to react icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 
 function SearchBar() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -26,28 +27,9 @@ function SearchBar() {
           id="default-search"
           type="text"
         />
-        {/* <button
-          type="submit"
-          className="text-white absolute right-2.5 bottom-1 bg-[#F02262] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 "
-        >
-          Search
-        </button> */}
-        <div class="absolute right-2.5 bottom-3 flex items-center pl-3 pointer-events-none">
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
+
+        <div className="absolute right-2.5 bottom-3 flex items-center pl-3 pointer-events-none">
+          <BsSearch />
         </div>
       </div>
     </form>
