446024c3f826afaa95eaa3711014703ae330ea70
Aariyan Apu <aariyan.apu@gmail.com>
2022-12-30 17:05:05 +0600

    bug fixed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,24 +8,15 @@ import { Link } from 'react-router-dom';
 function VideoCard({ data: { items }, direction }) {
   const [video, setVideo] = useState(null);
 
-
-
   useEffect(() => {
     return () => setVideo(items);
   }, []);
 
   console.log(video);
 
-
   return (
     <div className="ml-3 ">
-
-
-<h1> hello</h1>
-<h2>{video[2]?.snippet.title }</h2>
-{video?.map((item)=>(<h1>{item.snippet.title}</h1>))}
-
-      {/* <motion.div
+      <motion.div
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{
@@ -34,7 +25,9 @@ function VideoCard({ data: { items }, direction }) {
           ease: [0, 0.71, 0.2, 1.01],
         }}
       >
-        <div className={`flex flex-wrap justify-center gap-3  lg:justify-start ${direction} `}>
+        <div
+          className={`flex flex-wrap justify-center gap-3  lg:justify-start ${direction} `}
+        >
           {video?.map((item, idx) => (
             <motion.div whileHover={{ scale: 1.05 }} className="flex" key={idx}>
               <div className="rounded-lg shadow-lg bg-white w-[320px]  ">
@@ -52,7 +45,7 @@ function VideoCard({ data: { items }, direction }) {
                     to={`/video/${item?.id.videoId}`}
                     className="text-gray-900 text-base font-medium mb-2"
                   >
-                    {item?.snippet.title.slice(0, 60)}
+                    {item?.snippet?.title.slice(0, 60)}
                     ...
                   </Link>
                   <Link
@@ -66,7 +59,7 @@ function VideoCard({ data: { items }, direction }) {
             </motion.div>
           ))}
         </div>
-      </motion.div> */}
+      </motion.div>
     </div>
   );
 }
